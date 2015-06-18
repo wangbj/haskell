560fe4909ddc31cd996cd3937158fef88b0aa724
Baojun Wang <wangbj@gmail.com>
2015-06-17 18:40:24 -0700

    Update 0005.hs

--- 0005.hs
+++ 0005.hs
@@ -1,59 +1,54 @@
 {-# LANGUAGE BangPatterns #-}
-
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
---import Data.ByteString.Builder
---import Data.ByteString.Builder.ASCII
+import Data.ByteString.Builder
 import Control.Monad.Primitive
 import Control.Monad
+import Data.Monoid
 import Data.List
 import Data.Char
+import System.IO (stdout)
 
 type Palin = V.Vector Int
 
-isPalinS x = reverse x == x
-
-dig x
-  | x >= '0' && x <= '9' = ord x - ord '0'
 
 fromBS = V.map dig . V.fromList . C.unpack
-
-isPalin x = go x 0 ( (pred . V.length) x)
-  where go v !s !e
-          | s > e = True
-          | otherwise = case v V.! s == v V.! e of
-            True -> go v (succ s) (pred e)
-            _ -> False
-                                      
-fromBSM :: C.ByteString -> IO (V.MVector (PrimState IO)  Int)
-fromBSM = V.unsafeThaw . fromBS
+  where dig x = ord x - ord '0'
 
 type PalinM =  V.MVector (PrimState IO) Int
 
+fixupM :: PalinM -> Int -> Int -> IO ()
+fixupM m i j
+  | i < 0 = return ()
+  | i == 0 = MV.read m i >>= \vi -> when (vi > 9 && i /= j) ( MV.write m j 1 )
+  | otherwise = do
+    let !i' = pred i
+        !j' = succ j
+    vi <- MV.read m i
+    vi' <- MV.read m i'
+    when (vi > 9) (MV.write m i 0 >> MV.write m j 0 >> MV.write m i' (succ vi') >> MV.write m j' (succ vi'))
+    fixupM m i' j'
+      
 nextPalinM :: PalinM -> IO ()
-nextPalinM x = go x 0 (pred . MV.length $ x)
-  where go m s e
-          | s >= e = return ()
+nextPalinM x = go x 0 (MV.length x - 1) False False
+  where go m !i !j !c u
+          | i >= j = when (c || not u) (MV.read m j >>= \v1 -> MV.write m j (succ v1) >> MV.write m i (succ v1) ) >> fixupM m j i
           | otherwise = do
-            v1 <- MV.read m s
-            v2 <- MV.read m e
+            v1 <- MV.read m i
+            v2 <- MV.read m j
+            let !i' = succ i
+                !j' = pred j
             case compare v1 v2 of
-              EQ -> go m (succ s) (pred e)
-              GT -> MV.write m e v1 >> go m (succ s) (pred e)
-              LT -> do
-                let s' = succ s
-                    e' = pred e
-                v1' <- MV.read m s'
-                if (s < e && v1' < 9) then do
-                  if s' < e' then MV.write m e v1 else MV.write m s (succ v1) >> MV.write m e (succ v1)
-                  else do
-                  MV.write m s v2
-                go m (succ s) (pred e)
-        go :: PalinM -> Int -> Int -> IO ()
+              EQ -> go m i' j' c u
+              GT -> MV.write m j v1 >> go m i' j' False True
+              LT -> MV.write m j v1 >> go m i' j' True True
+        go :: PalinM -> Int -> Int -> Bool -> Bool -> IO ()
 
 c1 = C.pack "2134"
-            
+c2 = C.pack "94187978322"
+c3 = C.pack "214633"
+
 nextPalinMut :: C.ByteString -> IO Palin
 nextPalinMut c = do
   let v1 = fromBS c
@@ -62,9 +57,15 @@ nextPalinMut c = do
   v3 <- V.unsafeFreeze v2 :: IO Palin
   return v3
 
+printPalin :: Palin -> IO ()
+printPalin x = hPutBuilder stdout (go x 0 (V.length x))
+  where go v 0 l = if (l == 1 && (v V.! 0 > 9) ) then string8 "11\n" else intDec (v V.! 0) <> go v 1 l
+        go v !i l
+          | i >= l = char8 '\n'
+          | otherwise = intDec (v V.! i) <> go v (succ i) l
+
 doNextPalin_ :: C.ByteString -> IO ()
-doNextPalin_ s = nextPalinMut s >>= \r ->
-  (putStrLn . V.toList . V.map (\x -> chr (x + ord '0'))) r
+doNextPalin_ s = nextPalinMut s >>= printPalin
 
 getinputs s = C.lines rest
   where (_, t1) = C.break (== '\n') s
