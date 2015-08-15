60e4774e21fadeeea9bcbf030940d9a9c8fe38c3
Baojun Wang <wangbj@gmail.com>
2015-08-14 23:04:38 -0700

    Create busyman.hs
    I'm lazy with IO (optimization)

---
+++ busyman.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE BangPatterns #-}
+import qualified Data.ByteString.Char8 as C
+import qualified Data.IntMap as IntMap
+import Data.IntMap(IntMap)
+import Data.Maybe
+import Data.List
+import System.IO.Unsafe
+
+readpair s = fromJust $ C.readInt s >>= \(x, s1) ->
+  (C.readInt . C.tail) s1 >>= \(y, s2) ->
+  return $! (x, y)
+
+readint = fst . fromJust . C.readInt
+
+dbg :: (Show a) => a -> ()
+dbg = unsafePerformIO . print
+
+busy = fst . foldl go (1, (-1, maxBound))
+  where go (!r, !pp@(ppx, ppy)) p@(px, py)
+          | ppx == px = (r, pp) -- already sorted
+          | ppy <= px = (succ r, p)
+          | py <= ppy = (r, p)
+          | otherwise = (r, pp)
+
+processinputs 0 _ = []
+processinputs n (k:s) = busy ps : processinputs (pred n) rest
+  where (s1, rest) = splitAt (readint k) s
+        ps = IntMap.assocs . IntMap.fromListWith min . map readpair $ s1
+
+processall txts = processinputs (readint nt) rest
+  where (nt:rest) = C.lines txts
+
+pr = foldr p1 ""
+  where p1 x = shows x . showString "\n"
+
+main = C.getContents >>= putStr . pr . processall
