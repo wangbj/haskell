fe9be0e7a55de4106f272d093f39ff1068880021
Baojun Wang <wangbj@gmail.com>
2016-02-15 13:32:18 -0800

    Update nicetre.hs

--- nicetre.hs
+++ nicetre.hs
@@ -1,28 +1,17 @@
--- | 
-
-{-# LANGUAGE OverloadedStrings #-}
-
-module Main where
-
 import qualified Data.ByteString.Char8 as C
 import Text.ParserCombinators.ReadP
-import Control.Applicative
-import Data.Char
-
-n = char 'n'
-l = char 'l'
 
 leaf = do
-  l
+  char 'l'
   return 0
   
 branch = do
-  n
+  char 'n'
   x <- preorder
   y <- preorder
   return $! 1 + max x y
   
-preorder = leaf <|> branch
+preorder = leaf +++ branch
 
 valid = do
   d <- preorder
