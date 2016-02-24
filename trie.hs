645679e16e464849737dd5ad04d4877dc234295d
Baojun Wang <wangbj@gmail.com>
2016-02-23 20:22:47 -0800

    Update trie.hs

--- trie.hs
+++ trie.hs
@@ -1,53 +1,90 @@
-module Trie (
-    lookup
-  , insert
-  , insert'
-  , insertWith
-  , toList
-  ) where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ViewPatterns #-}
 
-import qualified Data.Map as Map
-import Data.Map(Map)
-import Data.Maybe
-import Control.Monad
+module Main where
+
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Foldable as F
+import qualified Data.Sequence as Seq
+import Data.Sequence(Seq)
+import qualified Data.IntMap.Strict as IntMap
+import Data.IntMap.Strict(IntMap)
+import Data.ByteString.Builder
+import Control.Applicative hiding (empty)
+import Control.Arrow
+import Control.Monad.Writer
+import Data.Monoid
 import Data.Char
-import Prelude hiding (lookup)
+import System.IO (stdout)
+import Data.Maybe
 
-data Trie a = Trie {
-    value :: Maybe a
-  , children :: Map Char (Trie a)
-  } deriving (Show, Eq)
+newtype Trie s a = Trie {
+  unTrie :: (FiniteStream s) => IntMap (Maybe a, Trie s a)
+  } 
 
-empty = Trie Nothing Map.empty
-singleton c = insert [c] empty
+empty = Trie IntMap.empty
 
-lookup [] (Trie v _) = True
-lookup (c:cs) t@(Trie val child) = case Map.lookup c child of
+class (Monoid s) => FiniteStream s where
+  type FiniteStreamElementType s :: *
+  uncons :: s -> Maybe (FiniteStreamElementType s, s)
+  isEmpty :: s -> Bool
+  fromListS :: [FiniteStreamElementType s] -> s
+  toListS :: s -> [FiniteStreamElementType s]
+  fromKey :: Int -> FiniteStreamElementType s
+  toKey :: FiniteStreamElementType s -> Int
+
+instance FiniteStream C.ByteString where
+  type FiniteStreamElementType C.ByteString = Char
+  uncons = C.uncons
+  isEmpty = C.null
+  fromListS = C.pack
+  toListS = C.unpack
+  fromKey = chr
+  toKey = ord
+  
+instance (Bounded a, Integral a) => FiniteStream [a] where
+  type FiniteStreamElementType [a] = a
+  uncons [] = Nothing
+  uncons (x:xs) = Just (x, xs)
+  isEmpty [] = True
+  isEmpty _ = False
+  fromListS = id
+  toListS = id
+  fromKey = fromIntegral
+  toKey = fromIntegral
+
+member (uncons -> Nothing) _ = True
+member (uncons -> Just (c, cs)) (Trie t) = case IntMap.lookup c t of
   Nothing -> False
-  Just child' -> lookup cs child'
+  Just (_, tr) -> member cs tr
 
-incr m = return $! case m of
-  Nothing -> 1
-  Just x -> succ x
+insertWith f (uncons -> Nothing) v t = t
+insertWith f (uncons -> Just (c, cs)) v (Trie t)
+  | isEmpty cs = case IntMap.lookup k t of
+    Nothing -> Trie $ IntMap.insert k (Just v, empty) t
+    Just _ -> Trie $ IntMap.update (Just . first (\x -> case x of
+                                                      Nothing -> Just v
+                                                      Just x' -> Just (f v x'))) k t
+  | otherwise = case IntMap.lookup k t of
+    Nothing -> Trie $ IntMap.insert k (Nothing, insertWith f cs v empty) t
+    Just _ -> Trie $ IntMap.update (Just . fmap (insertWith f cs v) ) k t
+  where k = c
+          
 
-decr m = return $! case m of
-  Nothing -> Nothing
-  Just x -> case x of
-    1 -> Nothing
-    x' -> Just $! pred x'
+insert s v = insertWith const s v
+singleton k v = insertWith const k v empty
 
-insert_ [] t = t
-insert_ (c:cs) t@(Trie val child) = case Map.lookup c child of
-  Nothing -> t { children = Map.insert c (insert_ cs empty) child, value = incr val }
-  Just child'@(Trie v _) -> t {children = Map.insert c (insert_ cs child') child, value = incr val }
+cons m t = mapM_ (uncurry (consM m)) (IntMap.assocs (unTrie t))
+consM m k (Nothing, t) = cons (m<>pure (k)) t
+consM m k (Just v, t) = let m' = m <> pure (k)
+                        in tell (Seq.singleton (F.toList m', v)) >> cons m' t
 
-insertWith g s t@(Trie val child) = insert_ (g s) t
+assocs t = F.toList $ execWriter (cons Seq.empty t)
 
-insert = insertWith id
-insert' = insertWith (\s -> s ++ [nil])
-  where nil = chr 0
-
-toList t@(Trie val child)
-  | Map.null child = [[]]
-  | otherwise =  Map.foldrWithKey go [] child
-  where go c t r = fmap (c:) (toList t) ++ r
+instance (FiniteStream s, Show a) => Show (Trie s a) where
+  show t = show $ execWriter (cons Seq.empty t)
